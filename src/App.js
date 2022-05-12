0bfeae69a13d04bee75774bba12c742c0d5838ed
MauricioAMartinez <mauricio.m@montechelo.com.co>
2022-05-12 11:35:53 -0500

    Finalizado

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import Character from './components/Character';
 import imageRickMorty from './img/rick-morty.png'
 function App() {
 
-  const [character,setCharacters] = useState(null);
+  const [characters,setCharacters] = useState(null);
 
   const reqApi = async () => {
     const api = await fetch('https://rickandmortyapi.com/api/character')
     const characterApi = await api.json();
     setCharacters(characterApi.results)
   }
-  console.log(character)
-
   return (
     <div className="App">
       <header className="App-header">
        <h1 className='title' >Rick & Morty</h1>
-       <Character/>
-       <img src={imageRickMorty} alt="Rick & Morty" className='img-home' ></img>
-       <button onClick={reqApi}  className='btn-search' >Buscar Personajes</button>
+       {characters ? (
+        <Character characters={characters} setCharacters={setCharacters} />
+       ):(
+         <>
+        <img src={imageRickMorty} alt="Rick & Morty" className='img-home' ></img>
+        <button onClick={reqApi}  className='btn-search' >Buscar Personajes</button>
+        </>
+       )}
+       
       </header>
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,47 @@
-export default function Character() {
+export default function Character(props) {
+  const { characters,setCharacters } = props;
+
+  const resetCharacters = () =>{
+    setCharacters(null);
+  }
   return (
-    <div>
-        <h1>Desde Character Js</h1>
+    <div className="characters">
+      <h1>Personajes</h1>
+      <span  onClick={resetCharacters} className="back-home">volver al home</span>
+      <div className="container-characters">
+        {characters.map((character, index) => (
+          <div className="character-container" key={index}>
+            <div>
+              <img src={character.image} alt={character.name} />
+            </div>
+            <div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status === "Alive" ? (
+                  <>
+                    <span className="alive" />
+                    Alive
+                  </>
+                ) : (
+                  <>
+                    <span className="dead" />
+                    Dead
+                  </>
+                )}
+              </h6>
+              <p className="text-grey" >
+                 <span>Episodios: </span> 
+                 <span> {character.episode.length} </span>
+               </p>
+               <p>
+                 <span className="text-grey" >Especie: </span>
+                 <span> {character.species} </span>
+               </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <span onClick={resetCharacters} className="back-home" >volver al home</span>
     </div>
-  )
+  );
 }
